8286738d418ee4a0560d89065ce051af78d28026
raha <r.fahadi1368@gmail.com>
2021-05-14 13:14:10 +0430

    Launch service descovery

---
+++ src/test/java/com/raha/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raha.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
